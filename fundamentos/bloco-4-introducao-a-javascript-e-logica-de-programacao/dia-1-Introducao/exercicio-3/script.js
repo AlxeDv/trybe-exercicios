cab51e987d2b3be6d2cbdca9990dc138d21b2f3a
Alex <alexmaia.etel@gmail.com>
2021-07-28 19:40:01 -0300

    Adiciona o exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-Introducao/exercicio-3/script.js
@@ -0,0 +1,11 @@
+let notaTecnica = 85;
+if (notaTecnica >= 80){
+    console.log("Parabéns, você foi aprovado!");
+}
+else if (notaTecnica < 80 && notaTecnica > 60) {
+    console.log("Você está na nossa lista de espera");
+} 
+else {
+    console.log("Você foi reprovado");
+
+}
