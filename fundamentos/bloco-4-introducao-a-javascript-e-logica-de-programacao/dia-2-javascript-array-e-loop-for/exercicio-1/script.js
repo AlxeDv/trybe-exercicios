0436d5f6f6dcaa18e3a0a113213374b9516a5f2f
alxedv <alexmaia.etel@gmail.com>
2021-07-29 19:39:04 -0300

    Adiciona os exercicios 2, 3, 4, 5 e 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-1/script.js
@@ -1,2 +1,52 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-console.log(numbers)
+// console.log(numbers) Exercício 1
+
+//exercicio 2
+/*let soma = 0
+for (let contador = 0; contador < numbers.length; contador+=1){
+     soma = soma + numbers[contador]
+}
+console.log(soma)*/
+
+//exercicio 3
+/*let soma = 0;
+let media;
+for (let contador = 0; contador < numbers.length; contador+=1){
+     soma = soma + numbers[contador];
+     media = soma / numbers.length
+}
+console.log(media)*/
+
+//exercicio 4
+/*let soma = 0;
+let media;
+for (let contador = 0; contador < numbers.length; contador+=1){
+     soma = soma + numbers[contador];
+     media = soma / numbers.length
+}
+if(media > 20){
+    console.log("Valor maior que 20.");
+} else {
+    console.log("Valor menor ou igual a 20");
+}*/
+
+//exercicio 5
+/*let numeroMaior = 0;
+for (let contador = 0; contador < numbers.length; contador+=1){
+    if (numeroMaior < numbers[contador]){
+        numeroMaior = numbers[contador]
+    }
+}
+console.log("O maior número deste array é:", numeroMaior)*/
+
+//exercicio 6
+/*let numeroImpar = 0;
+for (contador = 0; contador < numbers.length; contador+=1){
+    if(numbers[contador] % 2 != 0){
+        numeroImpar = numeroImpar + 1;
+    }
+}
+console.log("A quantidade de numeros Impares é:", numeroImpar)*/
+
+//exercicio 7
+
