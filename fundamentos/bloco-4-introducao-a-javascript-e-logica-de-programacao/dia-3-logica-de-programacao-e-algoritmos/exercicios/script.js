f9720ca7ec5ae9612bb9efbd14d21bd310f1e19a
alxedv <alexmaia.etel@gmail.com>
2021-07-30 19:10:30 -0300

    Adiciona os exercicios do 1 ao 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/exercicios/script.js
@@ -0,0 +1,35 @@
+/*let n = 5;
+let asterisco;
+
+for(let index = 0; index < n; index+=1){
+    asterisco = "*".repeat(n)
+    console.log(asterisco)
+}*/
+
+/*let n = 5;
+let asterisco;
+for (let index = 1; index < n+1; index+=1){
+    asterisco = "*".repeat(index)
+    console.log(asterisco)
+}*/
+
+/*let n = 5;
+let asterisco;
+let espaco;
+for (let index = 1; index < n+1; index+=1){
+    espaco = " ".repeat(n - index)
+    asterisco = "*".repeat(index)
+    console.log(espaco, asterisco)
+}*/
+
+/*let n = 5;
+let asterisco;
+let espaco;
+let secondIndex = 3;
+for (let index = 1; index < n+1; index+=1){
+    espaco = " ".repeat(n - secondIndex);
+    asterisco = "*".repeat(index);
+    index = index + 1;
+    secondIndex = secondIndex + 1;
+    console.log(espaco, asterisco, espaco);
+}*/
