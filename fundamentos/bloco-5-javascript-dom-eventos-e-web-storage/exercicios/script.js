400b5aa175315563f17688c93263a58e4e799acb
alxedv <alexmaia.etel@gmail.com>
2021-08-11 16:44:36 -0300

    Adiciona a tag main

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/exercicios/script.js
@@ -1,4 +1,8 @@
 let h1 = document.createElement("h1");
-h1.innerText = "Exercicio 5.2 - JavaScript DOM"
-document.body.append(h1)
-console.log(document.body.querySelector("h1").parentNode);
+h1.innerText = "Exercicio 5.2 - JavaScript DOM";
+document.body.append(h1);
+
+let main = document.createElement("main");
+main.className = "main content"
+document.body.appendChild(main);
+
