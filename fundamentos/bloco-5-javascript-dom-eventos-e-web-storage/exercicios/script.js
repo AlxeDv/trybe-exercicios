f36e262ddebfd31ea2275b45eb7d20f9c91f5639
alxedv <alexmaia.etel@gmail.com>
2021-08-11 17:24:24 -0300

    Adiciona a lista dentro de right-content

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/exercicios/script.js
@@ -14,3 +14,38 @@ let parag = document.createElement("p");
 parag.innerText = "Teste de parágrafo.";
 document.querySelector(".center-content").appendChild(parag)
 
+let leftSection = document.createElement("section");
+leftSection.className = "left-content";
+document.querySelector("main").appendChild(leftSection);
+
+let rightSection = document.createElement("section");
+rightSection.className = "right-content";
+document.querySelector("main").appendChild(rightSection);
+
+let smallImage = document.createElement("img");
+smallImage.src = "https://picsum.photos/200";
+smallImage.className = "small-image";
+document.querySelector(".left-content").appendChild(smallImage);
+
+let listItem = [
+    "um",
+    "dois",
+    "três",
+    "quatro",
+    "cinco",
+    "seis",
+    "sete",
+    "oito",
+    "nove",
+    "dez"
+]
+let lista = document.createElement("ul");
+document.querySelector(".right-content").appendChild(lista);
+for(let index = 0; index < listItem.length; index += 1){
+    let itens = listItem[index]
+
+    let itensLista = document.createElement("li")
+    itensLista.innerText = itens;
+    lista.appendChild(itensLista)
+}
+
